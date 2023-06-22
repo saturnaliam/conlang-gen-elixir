9e13e002006bb9ba351fd0dc8fa85a16b5a8dbe2
saturnaliam <llcsaturnaliam@gmail.com>
2023-06-22 11:22:11 -0500

    added writing system gen

--- lib/cg.ex
+++ lib/cg.ex
@@ -1,6 +1,37 @@
 defmodule CG do
   def generate_conlang do
-    langs = ["english", "spanish"]
-    IO.puts("#{Enum.random(langs)}")
+    gen_system = fn input, options ->
+      if input == "y" do
+        Enum.random(options)
+      else
+        :empty
+      end
+    end
+
+    writing =
+      IO.gets("Should I generate a writing system? [Y/N] ")
+      |> String.downcase()
+      |> String.trim("\n")
+      |> gen_system.(["Abugida", "Abjad", "Alphabet", "Logosyllabary", "Syllabary"])
+
+    IO.puts(writing)
+  end
+
+  def prompt_input do
+    user_input = IO.gets("> ") |> String.trim("\n")
+
+    case user_input do
+      "gen" ->
+        generate_conlang()
+
+      "exit" ->
+        exit(:exit)
+
+      _ ->
+        IO.puts("Unknown command #{user_input}")
+        prompt_input()
+    end
+
+    prompt_input()
   end
 end
